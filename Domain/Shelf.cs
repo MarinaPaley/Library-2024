9614b699c0e73e7924dc3496ed3cf44acf20e96b
Марина Васильева <marina_paley@mail.ru>
2024-09-16 19:52:36 +0300

    Написан минимальный функционал класса Полка

---
+++ Domain/Shelf.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Headers;
+
+namespace Domain
+{
+    public sealed class Shelf : IEquatable<Shelf>
+    {
+        private string name;
+
+        public Shelf(string name)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = name;
+        }
+
+        public Guid Id { get; }
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                this.name = value ?? throw new ArgumentNullException(nameof(value));
+            }
+        }
+
+        public bool Equals(Shelf? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.Name == other.Name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Shelf);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+
+        public override string ToString() => $"Название полки {this.Name}";
+    }
+}
---
+++ DomainTest/ShelfTests.cs
@@ -0,0 +1,48 @@
+using Domain;
+
+namespace DomainTest
+{
+    public class ShelfTests
+    {
+        [Test]
+        public void Ctor_ValidData_Success()
+        {
+            Assert.DoesNotThrow(() => _ = new Shelf("Полка 1"));
+        }
+
+        [Test]
+        public void Ctor_NullData_ExpectedException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _ = new Shelf(null!));
+        }
+
+        [TestCase("1", "1", true)]
+        [TestCase("1", "2", false)]
+        public void Equals_ValidData_Success(string name1, string name2, bool expected)
+        {
+            // arrange
+            var shelf1 = new Shelf(name1);
+            var shelf2 = new Shelf(name2);
+
+            // act
+            var actual = shelf1.Equals(shelf2);
+
+            // assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ToString_ValidData_Success()
+        {
+            // arrange
+            const string expected = "Название полки Полка 1";
+            var shelf = new Shelf("Полка 1");
+
+            // act
+            var actual = shelf.ToString();
+
+            // assert
+            Assert.That(actual, Is.EqualTo(expected) );
+        }
+    }
+}
